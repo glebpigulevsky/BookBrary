3780e44e2980fd7d46ce834cf293fd0cadc31974
CasperCarver <hleb.piguleuski@gmail.com>
2020-07-19 14:56:07 +0300

    fix: package.json scripts

--- server.js
+++ server.js
@@ -86,13 +86,13 @@ app.use('/api/photo', require('./routes/api/photo'));
 app.use('/api/tags', require('./routes/api/tags'));
 
 // Server static assets in production
-if (process.env.Node_ENV === 'production') {
-  //Set static folder
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;

e8eb447e58f5109131b9cf2d98353f54d9e7e30a
CasperCarver <hleb.piguleuski@gmail.com>
2020-07-19 21:30:37 +0300

    fix: fixing server assets

--- server.js
+++ server.js
@@ -90,13 +90,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res, next) => {
-    if (
-      req.headers['x-forwarded-proto'] != 'https' &&
-      process.env.NODE_ENV === 'production'
-    )
-      res.redirect(req.protocol + req.hostname + req.url);
-    else res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

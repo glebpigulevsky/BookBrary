c695c423e703eb26deec9d2083133f537f8bad68
CasperCarver <hleb.piguleuski@gmail.com>
2020-07-20 08:54:12 +0300

    fix: fixing path static server

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get(['*', '!/image'], (req, res) => {
+  app.get(['*', '!./image'], (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
